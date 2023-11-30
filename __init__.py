4b148c7497f181b8332ee015dc3af62f93d8deeb
JakkoT <jakkoturro@gmail.com>
2023-11-30 12:45:43 +0200

    download

--- __init__.py
+++ __init__.py
@@ -9,3 +9,12 @@ app = Flask(__name__)
 @app.route("/")
 def test():
 	return render_template("index.html")
+
+@app.route('/download')
+def download_file():
+    file_path = 'files/test.txt'  # Replace with the actual path to your file
+    return send_file(file_path, as_attachment=True)
+
+@app.route('/')
+def index():
+    return render_template('download.html')
